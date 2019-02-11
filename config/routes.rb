616fdc629b4cc6ca90d1c1f373b656978f1fd870
kamal <a2ninek@yahoo.com>
2019-02-11 12:03:42 +0500

    routes fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+  as :user do
+    get "/users/sign_up" => "devise/registrations#new", constraints: { subdomain: 'office' }
+    post 'users' => 'devise/registrations#create', :as => 'user_registration', constraints: { subdomain: 'office' }
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/affiliate_program' => 'affiliate_programs#index'
   get '/' => 'landing_page#index', as: :home
