c8236bf57cc7f5623248df3e8bf0a805696a08bd
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-02-14 14:45:21 +0500

    Fixed Issues and added FAQ and Terms page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/affiliate_program' => 'affiliate_programs#index'
+  get '/terms' => 'terms#index'
+  get '/faq' => 'faq#index'
+  get '/download' => 'downloads#index', as: :download
   get '/' => 'landing_page#index', as: :home
 end
