5b8fa9755368fdc55f9e995546060707dff12467
kamal <a2ninek@yahoo.com>
2019-02-15 20:43:25 +0500

    fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,8 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
   def update_node
     if resource.persisted?
-      debugger
-      puts "its here", "\n" * 5
       dummy_user.children.update(parent_id: resource.id)
       resource.update(parent_id: dummy_user.parent_id, parent_position: dummy_user.parent_position, created_by_id: dummy_user.created_by_id)
       dummy_user.destroy
