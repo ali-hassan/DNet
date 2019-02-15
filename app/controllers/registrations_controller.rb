af635851f75d4f90b56947f09619817d2a0ad981
kamal <a2ninek@yahoo.com>
2019-02-15 21:16:40 +0500

    fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   after_action :update_node, only: [:create]
   private
   def update_node
-    if resource.persisted?
+    if resource.persisted? && resource.sponsor_id
       dummy_user.children.update(parent_id: resource.id)
       resource.update(parent_id: dummy_user.parent_id, parent_position: dummy_user.parent_position, created_by_id: dummy_user.created_by_id)
       dummy_user.destroy
