41d48f931b1d8a0867211c68b1106d6ea0231f69
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-02-16 18:55:18 +0500

    Bug: Fixed issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
                                              :document_number, :first_name, :last_name])
   end
   def after_sign_in_path_for(resource)
-    if !resource.is_admin? && !resource.is_package_activated?
+    if resource.class.name != "AdminUser" && !resource.is_admin? && !resource.is_package_activated?
       buy_planes_path
     else
       request.env['omniauth.origin'] || stored_location_for(resource) || root_path
