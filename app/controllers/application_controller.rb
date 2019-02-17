7d7bd552d7f98b05577163db3d3ad62224782874
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-02-17 19:46:01 +0500

    Changes in design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_params, if: :devise_controller?
+  before_action :check_subdomain
   protected
   def configure_permitted_params
     devise_parameter_sanitizer.permit(:sign_up,
@@ -12,6 +13,11 @@ class ApplicationController < ActionController::Base
       buy_planes_path
     else
       request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    end
+  end
+  def check_subdomain
+    if request.subdomain == "office" && ["/", "/affiliate_program"].include?(request.path)
+      redirect_to request.url.sub("office.", "")
     end
   end
 
