91b66788d180a49264f6818ebe481497455e396c
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-02-18 10:48:20 +0500

    Changes in code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,39 +27,29 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
   config.assets.debug = true
-
-  # Suppress logger output for asset requests.
   config.assets.quiet = true
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
-      user_name:            '',
-      password:             '',
-      authentication:       :plain,
-      enable_starttls_auto: true
+      user_name:            'iammlmadvisor@gmail.com',
+      password:             'Pakistan.1234',
+      authentication:       :login,
+      # enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
