cb651be330106b7ec61bc694c839f424c03ec12d
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-02-18 18:03:02 +0500

    Fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ApplicationMailer
   def contact_us(params)
-    @email = 'support@forexhometrade.com'
+    @sender = 'support@forexhometrade.com'
 
     @name     = params[:name]
     @message  = params[:body]
     @phone    = params[:phone]
     @subject  = params[:subject]
     @email    = params[:email]
-    mail(to: @email, subject: "Contact us email | #{@subject} | #{@name}")
+    mail(to: @sender, subject: "Contact us email | #{@subject} | #{@name}")
   end
 end
