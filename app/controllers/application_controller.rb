541bdbb459c5e67bc99b17b3d425bdec7c750ea4
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-03-04 12:02:55 +0500

    Skipped Authnticity token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
+
   before_action :configure_permitted_params, if: :devise_controller?
   before_action :check_subdomain
   protected
