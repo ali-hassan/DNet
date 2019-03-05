c28ee826fc9e5ebdf16392b2a5bfca89853a4dc8
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-03-05 23:47:44 +0500

    Changes And fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,5 +77,5 @@ class User < ApplicationRecord
   end
   delegate :find_last_right_node, :find_last_left_node, :parent_lists, :package_price, :direct_bonus_users_count,
    :direct_bonus_users_count_left, :direct_bonus_users_count_right, :indirect_bonus_users_count, :earn_weekly_point,
-   :left_team_members_count, :right_team_members_count, :current_package, :children_list, :binary_bonus, :cash_wallet_total, :total_income, to: :adapter
+   :left_team_members_count, :right_team_members_count, :current_package, :children_list, :binary_bonus, :cash_wallet_total, to: :adapter
 end
