bde915956cb1b3cfb19d5420c69d4e06571cba6a
kamal <a2ninek@yahoo.com>
2019-03-11 17:54:03 +0500

    fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,6 +26,6 @@ module ApplicationHelper
     users_sign_up_url(refered_name: current_user.username, position: position)
   end
   def plan_polymorphic_path(id)
-    current_user.adapter.can_upgrade_url?(id.to_s) && send("#{controller.controller_name.singularize}_url", id, subdomain: 'office') || "#"
+    (controller.controller_name == "buy_plans" || current_user.adapter.can_upgrade_url?(id.to_s)) && send("#{controller.controller_name.singularize}_url", id, subdomain: 'office') || "#"
   end
 end
