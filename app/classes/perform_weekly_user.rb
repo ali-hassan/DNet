f7fd95db6d0a79ec97907135354a5690edf7ecb3
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-03-17 21:15:44 +0500

    Changes inc doe

--- app/classes/perform_weekly_user.rb
+++ app/classes/perform_weekly_user.rb
@@ -16,7 +16,7 @@ class PerformWeeklyUser
     calculate_condition && calculate || true
   end
   def calculate_condition
-    (current_package_iteration > 0) && (adapter.max_package_total_earning > current_x_factor_income.try(:to_f))
+    (current_package_iteration > 0) && (adapter.max_package_total_earning >= current_x_factor_income.try(:to_f))
   end
   def calculate
     update(params); log_weekly_roi; decrement!(:current_package_iteration); check_for_next_week
