e754df4bc8cf0c2be717988254c9b6dcc29eaca6
kamal <a2ninek@yahoo.com>
2019-03-20 15:39:11 +0500

    fixes

--- app/workers/weekly_plan_bonus_worker.rb
+++ app/workers/weekly_plan_bonus_worker.rb
@@ -2,9 +2,11 @@ class WeeklyPlanBonusWorker
   include Sidekiq::Worker
 
   def perform(args)
-    @user = User.find_by(id: args["user_id"])
-    puts "*********************UserID which is going to take ROI: #{args["user_id"]}************"
-    puts "********************User Name: #{@user.username} ****************************"
-    @user.try(:adapter).try(:earn_weekly_point)
+    ActiveRecord::Base.uncached do
+      @user = User.find_by(id: args["user_id"])
+      puts "*********************UserID which is going to take ROI: #{args["user_id"]}************"
+      puts "********************User Name: #{@user.try(:username)} ****************************"
+      @user.try(:adapter).try(:earn_weekly_point)
+    end
   end
 end
