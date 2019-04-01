c6dabea3b2ae3b00d500a02c88f48375630d5dd8
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-04-01 12:45:45 +0500

    Default PIN option

--- app/admin/free_users.rb
+++ app/admin/free_users.rb
@@ -10,6 +10,7 @@ ActiveAdmin.register User, as: 'FreeUser' do
       f.input :username
       f.input :pin_capacity, label: "Pin Capacity", as: :select, collection: ["100$", "200$", "300$", "500$", "1000$", "2000$", "5000$", "10000$"], :prompt => "Select Pin Capacity"
       f.input :email
+      f.input :pin, as: :password
       f.input :password
       f.input :password_confirmation
       f.input :smart_wallet_balance
