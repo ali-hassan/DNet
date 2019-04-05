d6dd4b7b8fa0f3b6bca31ff159326acb98599d27
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-04-05 11:42:42 +0500

    Keys for captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
-  config.secret_key = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
+  config.site_key  = '6LepIZwUAAAAABYuq0FrAIA-wDB170hbgHBk2tai'
+  config.secret_key = '6LepIZwUAAAAAGJHjt0fRDTlUVG16zjHayyUqHIL'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
