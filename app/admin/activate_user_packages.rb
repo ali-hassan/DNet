5375c75e1b59f9db34bd2aeaaca6ff242387b4f1
kamal <a2ninek@yahoo.com>
2019-04-09 22:25:04 +0500

    fixes

--- app/admin/activate_user_packages.rb
+++ app/admin/activate_user_packages.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register ActivateUserPackage do
   form do |f|
     f.inputs do
       f.input :package_id, collection: FindPackages.new("100").packages.map { |k, hash| ["#{hash[:category]}-#{hash[:price]}", hash[:price] ] }
-      f.input :user_id, collection: User.all.map { |usr| [usr.username, usr.id] }
+      f.input :user, collection: User.all.map { |usr| [usr.username, usr.id] }
     end
     actions
   end
