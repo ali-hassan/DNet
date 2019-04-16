f14066804570ca107778b286dc53db78d60f312d
kamal <a2ninek@yahoo.com>
2019-04-16 22:00:45 +0500

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190415134003) do
+ActiveRecord::Schema.define(version: 20190416154634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,7 @@ ActiveRecord::Schema.define(version: 20190415134003) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "pending"
+    t.string "service"
     t.index ["user_id"], name: "index_withdrawl_requests_on_user_id"
   end
 
