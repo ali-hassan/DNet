4f5790ad4d442b834fed233689ce391f0b0dfa65
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-05-21 13:23:38 +0500

    Fixed Presentation typo mistake as well

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,7 +1,7 @@
 class DownloadsController < ApplicationController
 
   def index
-    @filename = File.join(Rails.root, "public", "Prasentation.pdf")
+    @filename = File.join(Rails.root, "public", "Presentation.pdf")
     send_file(@filename ,
               :type => 'pdf',
               :disposition => 'attachment')
