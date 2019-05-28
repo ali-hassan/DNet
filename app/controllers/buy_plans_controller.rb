08c5c1c2fe88477cd6d9985342f0f1221ad5341c
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-05-29 01:01:04 +0500

    Bug: Fixed bug

--- app/controllers/buy_plans_controller.rb
+++ app/controllers/buy_plans_controller.rb
@@ -20,6 +20,6 @@ class BuyPlansController < ApplicationController
     false
   end
   def shift_to_pkg
-    current_user.update is_pin: false, binary_bonus: 0.0,  current_x_factor_income: 0  #, pin_capacity: nil
+    current_user.update is_pin: false, binary_bonus: 0.0, binary_bonus_for_xfactor: 0.0,  current_x_factor_income: 0  #, pin_capacity: nil
   end
 end
