597b1e33c0c8008a38e7fb4bac435f015279cb5a
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2019-08-23 19:01:16 +0500

    Fixes

--- app/controllers/withdrawl_requests_controller.rb
+++ app/controllers/withdrawl_requests_controller.rb
@@ -5,19 +5,19 @@ class WithdrawlRequestsController < ApplicationController
   layout "dashboard"
   before_action only: [:create] do
     @cash_wallet_message = Setting.find_by(key: "cash_wallet_message").value
-    if @is_cash_wallet.present?
+    if @cash_wallet_message.present?
       redirect_to(withdrawl_requests_url(subdomain: 'office'), notice: @cash_wallet_message)
-    elsif (current_user.adapter.cash_wallet_total < permitted_params[:pts].try(:to_f))
-      redirect_to(withdrawl_requests_url(subdomain: 'office'), notice: "You don't have enough money to make this request.")
-    else
-      current_hongkong_time = Time.current.in_time_zone('Hong Kong')
-      Time.zone = "Hong Kong"
-      if (current_user.withdrawl_date.present? && current_user.withdrawl_date.to_date.to_s(:db) == Date.today.to_s(:db))
-        redirect_to(withdrawl_requests_url(subdomain: 'office'), notice: 'Only one withdrawl allowed per day')
-      else
-        (current_hongkong_time.between?(Time.zone.parse("10:00am"), Time.zone.parse("7:00pm")) && current_hongkong_time.sunday?)  || redirect_to(withdrawl_requests_url(subdomain: 'office'), notice: 'Withdrawl is only allowed on Sunday from 10:00 AM to 7:00 PM time (GMT+8)')
-      end
-    end
+    # elsif (current_user.adapter.cash_wallet_total < permitted_params[:pts].try(:to_f))
+    #   redirect_to(withdrawl_requests_url(subdomain: 'office'), notice: "You don't have enough money to make this request.")
+    # else
+    #   current_hongkong_time = Time.current.in_time_zone('Hong Kong')
+    #   Time.zone = "Hong Kong"
+    #   if (current_user.withdrawl_date.present? && current_user.withdrawl_date.to_date.to_s(:db) == Date.today.to_s(:db))
+    #     redirect_to(withdrawl_requests_url(subdomain: 'office'), notice: 'Only one withdrawl allowed per day')
+    #   else
+    #     (current_hongkong_time.between?(Time.zone.parse("10:00am"), Time.zone.parse("7:00pm")) && current_hongkong_time.sunday?)  || redirect_to(withdrawl_requests_url(subdomain: 'office'), notice: 'Withdrawl is only allowed on Sunday from 10:00 AM to 7:00 PM time (GMT+8)')
+    #   end
+    # end
   end
   after_action only: [:create] do
     current_user.update bitcoin_url: permitted_params[:permitted_params]
